909f6e7cd9ec34367e558491b248134d06e16d62
Xyoyu <ken@slinky.link>
2021-07-24 20:33:40 +0800

    fix: broken text styles

--- src/__stories__/TextStyles.story.tsx
+++ src/__stories__/TextStyles.story.tsx
@@ -9,5 +9,11 @@ export default {
 export const _Text = () => (
   <VStack>
     <Text>Default Text</Text>
+    <Text textStyle="overline">Overline Text</Text>
+    <Text textStyle="label.sm">Label Small</Text>
+    <Text textStyle="label.md">Label Medium</Text>
+    <Text textStyle="heading.sm">Heading Small</Text>
+    <Text textStyle="heading.lg">Heading Large</Text>
+    <Text textStyle="heading.xl">CensorBot Heading</Text>
   </VStack>
 )
