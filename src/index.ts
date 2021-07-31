687fc76cc6fd6a203e03f5953a5adf7944721c80
Julian Berryessa <jb@jt3ch.net>
2021-07-31 19:13:33 -0400

    fix(views): Export missing components

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,16 @@
 import { CCProvider } from '@/utils/CCProvider/CCProvider'
+export * from '@/utils/CCProvider/CCProvider'
 
 export * from '@/theme'
+
+// Views
+
 export * from '@/views/Category/Category'
 export * from '@/views/Category/Option/CategoryOption'
+
 export * from '@/views/NavActions/NavActions'
-export * from '@/utils/CCProvider/CCProvider'
+
+export * from '@/views/Section'
+export * from '@/views/Option'
 
 export default CCProvider
