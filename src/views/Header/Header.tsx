4e410e57e9b5fdc370f9c843abc6f2141991119d
Xyoyu <ken@slinky.link>
2021-08-09 05:48:32 +0800

    fix: Header height

--- src/views/Header/Header.tsx
+++ src/views/Header/Header.tsx
@@ -2,15 +2,22 @@ import type { PropsWithChildren } from 'react'
 import { Box, HStack, Text } from '@chakra-ui/react'
 import { CensorBotIcon } from '@/views/Header/CensorBotIcon'
 
-const ICON_SIZE = '48px'
-const HEADER_HEIGHT = '7vh'
+const ICON_SIZE = '40px'
+const HEADER_HEIGHT = '110px'
 
 export interface HeaderProps extends PropsWithChildren<unknown> {
   title: string
 }
 
 export const Header = ({ title, children }: HeaderProps) => (
-  <HStack px={8} h={HEADER_HEIGHT} justify="space-between" w="full">
+  <HStack
+    px={8}
+    h={HEADER_HEIGHT}
+    justify="space-between"
+    w="full"
+    flexShrink={0}
+    flexGrow={0}
+  >
     <HStack spacing={4}>
       <CensorBotIcon boxSize={ICON_SIZE} />
       <Text as="h1" color="lighter.100" textStyle="heading.xl">
